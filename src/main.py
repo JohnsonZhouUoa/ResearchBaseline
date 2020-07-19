79ae07631c34c15165155783edf9cfb74ba13d03
johnson <zzho612@aucklanduni.ac.nz>
2020-07-20 03:30:19 +1200

    Edit classifier

--- src/main.py
+++ src/main.py
@@ -9,6 +9,7 @@ from skmultiflow.drift_detection import DDM
 from skmultiflow.data import ConceptDriftStream
 from skmultiflow.data import HyperplaneGenerator
 from skmultiflow.data import DataStream
+from sklearn import metrics
 
 # GLobal variable
 SAMPLE_SIZE = 5000
@@ -17,39 +18,39 @@ TMP_FOLDER = "/tmp/autosklearn_parallel_example_tmp"
 OUT_FOLER = "/tmp/autosklearn_parallel_example_out"
 
 
-def spawn_classifier(seed, X_train, y_train):
-    """Spawn a subprocess.
-    auto-sklearn does not take care of spawning worker processes. This
-    function, which is called several times in the main block is a new
-    process which runs one instance of auto-sklearn.
-    """
-
-    # Use the initial configurations from meta-learning only in one out of
-    # the four processes spawned. This prevents auto-sklearn from evaluating
-    # the same configurations in four processes.
-    if seed == 0:
-        initial_configurations_via_metalearning = 25
-    else:
-        initial_configurations_via_metalearning = 0
-
-    # Arguments which are different to other runs of auto-sklearn:
-    # 1. all classifiers write to the same output directory
-    # 2. shared_mode is set to True, this enables sharing of data between
-    # models.
-    # 3. all instances of the AutoSklearnClassifier must have a different seed!
-    automl = AutoSklearnClassifier(
-        time_left_for_this_task=60,  # sec., how long should this seed fit
-        # process run
-        per_run_time_limit=15,  # sec., each model may only take this long before it's killed
-        ml_memory_limit=1024,  # MB, memory limit imposed on each call to a ML algorithm
-        shared_mode=True,  # tmp folder will be shared between seeds
-        tmp_folder=TMP_FOLDER,
-        output_folder=OUT_FOLER,
-        delete_tmp_folder_after_terminate=False,
-        ensemble_size=0,  # ensembles will be built when all optimization runs are finished
-        initial_configurations_via_metalearning=initial_configurations_via_metalearning,
-        seed=seed)
-    automl.fit(X_train, y_train)
+# def spawn_classifier(seed, X_train, y_train):
+#     """Spawn a subprocess.
+#     auto-sklearn does not take care of spawning worker processes. This
+#     function, which is called several times in the main block is a new
+#     process which runs one instance of auto-sklearn.
+#     """
+#
+#     # Use the initial configurations from meta-learning only in one out of
+#     # the four processes spawned. This prevents auto-sklearn from evaluating
+#     # the same configurations in four processes.
+#     if seed == 0:
+#         initial_configurations_via_metalearning = 25
+#     else:
+#         initial_configurations_via_metalearning = 0
+#
+#     # Arguments which are different to other runs of auto-sklearn:
+#     # 1. all classifiers write to the same output directory
+#     # 2. shared_mode is set to True, this enables sharing of data between
+#     # models.
+#     # 3. all instances of the AutoSklearnClassifier must have a different seed!
+#     automl = AutoSklearnClassifier(
+#         time_left_for_this_task=60,  # sec., how long should this seed fit
+#         # process run
+#         per_run_time_limit=15,  # sec., each model may only take this long before it's killed
+#         ml_memory_limit=1024,  # MB, memory limit imposed on each call to a ML algorithm
+#         shared_mode=True,  # tmp folder will be shared between seeds
+#         tmp_folder=TMP_FOLDER,
+#         output_folder=OUT_FOLER,
+#         delete_tmp_folder_after_terminate=False,
+#         ensemble_size=0,  # ensembles will be built when all optimization runs are finished
+#         initial_configurations_via_metalearning=initial_configurations_via_metalearning,
+#         seed=seed)
+#     automl.fit(X_train, y_train)
 
 
 for dir in [TMP_FOLDER, OUT_FOLER]:
@@ -80,35 +81,35 @@ X, y = drift_stream.next_sample(BATCH_SIZE)
 
 print('Starting to build the initial classifier!')
 
-processes = []
-for i in range(4):  # set this at roughly half of your cores
-    p = multiprocessing.Process(target=spawn_classifier, args=(i, X, y))
-    p.start()
-    processes.append(p)
-for p in processes:
-    p.join()
+# processes = []
+# for i in range(4):  # set this at roughly half of your cores
+#     p = multiprocessing.Process(target=spawn_classifier, args=(i, X, y))
+#     p.start()
+#     processes.append(p)
+# for p in processes:
+#     p.join()
 
 automl = AutoSklearnClassifier(time_left_for_this_task=60,
                                per_run_time_limit=15,
                                ml_memory_limit=1024,
                                shared_mode=True,
-                               ensemble_size=0,
+                               ensemble_size=2,
                                tmp_folder=TMP_FOLDER,
                                output_folder=OUT_FOLER,
                                initial_configurations_via_metalearning=0,
                                seed=1)
 
 #TODO: open issues 856 and 868 on github
-#automl.fit(X, y)
-automl.fit_ensemble(y,
-                    task=MULTICLASS_CLASSIFICATION,
-                    metric=accuracy,
-                    precision='32',
-                    dataset_name='digits',
-                    ensemble_size=20,
-                    ensemble_nbest=50)
-print("The initial model is: ")
-print(automl.show_models())
+automl.fit(X, y)
+# automl.fit_ensemble(y,
+#                     task=MULTICLASS_CLASSIFICATION,
+#                     metric=accuracy,
+#                     precision='32',
+#                     dataset_name='digits',
+#                     ensemble_size=20,
+#                     ensemble_nbest=50)
+# print("The initial model is: ")
+# print(automl.show_models())
 
 
 #fhddm = FHDDM()
@@ -116,11 +117,12 @@ ddm = DDM()
 while drift_stream.has_more_samples():
     X_next, y_next = drift_stream.next_sample(BATCH_SIZE)
     y_predict = automl.predict(X_next)
+
     if len(y_next) != len(y_predict):
         print("Predictions length not correct")
         break
     for j in range(len(y_next)):
-        ddm.add_element(y_next[j] == y_predict[j])
+        ddm.add_element(y_next[j] != y_predict[j])
         if ddm.detected_warning_zone():
             print('Warning zone has been detected in data: ' + str(y_next[j]) + ' - of index: ' + str(j))
         if ddm.detected_change():
@@ -129,8 +131,7 @@ while drift_stream.has_more_samples():
                                            per_run_time_limit=15,
                                            ml_memory_limit=4096,
                                            shared_mode=True,
-                                           ensemble_size=10,
-                                           ensemble_nbest=50,
+                                           ensemble_size=0,
                                            tmp_folder=TMP_FOLDER,
                                            output_folder=OUT_FOLER,
                                            initial_configurations_via_metalearning=0,
--- src/test/ddmTest.py
+++ src/test/ddmTest.py
@@ -1,6 +1,8 @@
 import numpy as np
 import sklearn.datasets
 from skmultiflow.drift_detection import DDM
+from sklearn import linear_model
+import math
 
 ddm = DDM()
 
@@ -8,14 +10,27 @@ TEST_DATA_SIZE = 10000
 DRIFT_START = 5000
 
 # Simulating a data stream
-data_stream = np.random.randint(2, size=TEST_DATA_SIZE)
+# data_stream = np.random.randint(2, size=TEST_DATA_SIZE)
 # Add a concept drift at index 5000
-for i in range(DRIFT_START, TEST_DATA_SIZE):
-    data_stream[i] = np.random.randint(5, 10)
+# for i in range(DRIFT_START, TEST_DATA_SIZE):
+#     data_stream[i] = np.random.randint(5, 10)
+
+data_stream = np.array([x * 2 + 1 for x in range(1, TEST_DATA_SIZE+1)])
+
+for i in range(50, TEST_DATA_SIZE):
+    if i % 50 == 0:
+        for j in range(i, i+20):
+            data_stream[j] = j * 4 + 2
+
+regr = linear_model.LinearRegression()
+
+regr.fit(np.array([x for x in range(1, 21)]).reshape((-1, 1)), data_stream[0:20])
 
 # Adding stream elements to ddm and verifying if drift occurred
-for i in range(len(data_stream)):
-    ddm.add_element(data_stream[i])
+for i in range(21, len(data_stream)):
+    y_next = data_stream[i - 1]
+    y_predict = regr.predict(np.array([i]).reshape((-1, 1)))
+    ddm.add_element(math.floor(y_next) != math.floor(y_predict))
     if ddm.detected_warning_zone():
         print('Warning zone has been detected in data: ' + str(data_stream[i]) + ' - of index: ' + str(i))
     if ddm.detected_change():
